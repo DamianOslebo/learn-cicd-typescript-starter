e36a06583d2e1818ad287a9564bf11fcce357aab
DamianOslebo <damian.oslebo@gmail.com>
2026-03-23 19:15:47 -0400

    add unit test for getApiKey

---
+++ src/tests/auth.test.ts
@@ -0,0 +1,29 @@
+import { describe, expect, test } from "vitest";
+import { getAPIKey } from "../api/auth.js";
+import { IncomingHttpHeaders } from "http";
+
+const person = {
+  isActive: true,
+  age: 32,
+};
+
+describe("API Auth", () => {
+    const headers: IncomingHttpHeaders = {
+        "authorization": "ApiKey my-secret-key"
+    };
+
+    test("returns API key from headers", () => {
+        const apiKey = getAPIKey(headers);
+        expect(apiKey).toBe("my-secret-key");
+    });
+
+    test("returns null if authorization header is missing", () => {
+        const apiKey = getAPIKey({});
+        expect(apiKey).toBeNull();
+    });
+
+    test("returns null if authorization header is not in correct format", () => {
+        const apiKey = getAPIKey({ "authorization": "Bearer my-secret-key" });
+        expect(apiKey).toBeNull();
+    });
+});
