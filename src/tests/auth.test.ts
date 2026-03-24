525dbab6c5e3a577abf646abcb4ce5b2d594ac11
DamianOslebo <damian.oslebo@gmail.com>
2026-03-23 20:44:24 -0400

    check for style

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -8,22 +8,22 @@ const person = {
 };
 
 describe("API Auth", () => {
-    const headers: IncomingHttpHeaders = {
-        "authorization": "ApiKey my-secret-key"
-    };
+  const headers: IncomingHttpHeaders = {
+    authorization: "ApiKey my-secret-key",
+  };
 
-    test("returns API key from headers", () => {
-        const apiKey = getAPIKey(headers);
-        expect(apiKey).toBe("my-secret-key");
-    });
+  test("returns API key from headers", () => {
+    const apiKey = getAPIKey(headers);
+    expect(apiKey).toBe("my-secret-key");
+  });
 
-    test("returns null if authorization header is missing", () => {
-        const apiKey = getAPIKey({});
-        expect(apiKey).toBeNull();
-    });
+  test("returns null if authorization header is missing", () => {
+    const apiKey = getAPIKey({});
+    expect(apiKey).toBeNull();
+  });
 
-    test("returns null if authorization header is not in correct format", () => {
-        const apiKey = getAPIKey({ "authorization": "Bearer my-secret-key" });
-        expect(apiKey).toBeNull();
-    });
+  test("returns null if authorization header is not in correct format", () => {
+    const apiKey = getAPIKey({ authorization: "Bearer my-secret-key" });
+    expect(apiKey).toBeNull();
+  });
 });
